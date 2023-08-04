a714f83f026d47079fb8ffa4c217928d1fd833dd
daniel <chididaniel1998@gmail.com>
2023-08-04 10:24:55 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Body from './body';
+import './images/App.css'
+import Start from './start';
+import Nav from 'react-bootstrap/Nav';
+
+function App() {
+  return (
+    <>
+    <Start />
+    <Body/>
+    </>
+  );
+}
+
+export default App;
